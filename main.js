d61aa437a3aed8cc27d5c8692e32b3b4117a6674
saItist <tamerlanchapaev06062008@gmail.com>
2025-10-22 21:44:18 +0300

    fixing spaces using class list toggle method

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 const productCard = document.querySelectorAll('.card-container')
 const changeAllCardColorButton = document.querySelector('#change-all-cards-color-button')
 const goldenColorHash = '#FFD700'
-const blackColorHash = '#000000'
 
 changeAllCardColorButton.addEventListener('click', () => {
     productCard.forEach((card) => card.style.backgroundColor = goldenColorHash)
@@ -13,6 +12,7 @@ changeAllCardColorButton.addEventListener('click', () => {
 
 const firstProductCard = document.querySelector('.card-container')
 const changeFirstCardColorButton = document.querySelector('#change-first-card-color-button')
+const blackColorHash = '#000000'
 
 changeFirstCardColorButton.addEventListener('click', () => {
     firstProductCard.style.backgroundColor = blackColorHash;
@@ -24,9 +24,9 @@ const openGoogleButton = document.querySelector('#open-google');
 openGoogleButton.addEventListener('click', openGoogle)
 
 function openGoogle() {
-    const answer = confirm('Вы действительно хотите открыть Google?');
-    if(answer=== true){
-        window.open('https://www.google.com')
+    const answer = confirm ('Вы действительно хотите открыть Google?') ;
+    if (answer=== true) {
+        window.open ('https://www.google.com')
     } else {
         return;
     }
@@ -35,33 +35,27 @@ function openGoogle() {
 // Вывод сообщения в консоль лог и алерт
 const outputLogButton = document.querySelector('#output-console-log-button')
 
-outputLogButton.addEventListener('click',() => outputConsoleLog('ДЗ №4'))
+outputLogButton.addEventListener ('click', () => outputConsoleLog('ДЗ №4'))
 
-function outputConsoleLog(message) {
-    alert(message)
-    console.log(message)
+function outputConsoleLog (message) {
+    alert (message)
+    console.log (message)
 }
-const mainTitle = document.querySelector('h1');
 
 // Вывод текста заголовка в консоль при наведении мыши
-mainTitle.addEventListener('mouseover', () => {
+
+const mainTitle = document.querySelector('h1');
+
+mainTitle.addEventListener ('mouseover', () => {
   console.log(mainTitle.textContent);
 });
+
 // Кнопка меняет цывет при каждом клике
 const toggleButton = document.querySelector('#toggle-button-color');
 
-const silverColor = '#C0C0C0';
-const goldColor = '#FFD700';
-
-let isSilver = true; // флаг состояния
-
-toggleButton.style.backgroundColor = silverColor;
+toggleButton.classList.add('silver');
 
 toggleButton.addEventListener('click', () => {
-    if (isSilver) {
-        toggleButton.style.backgroundColor = goldColor;
-    } else {
-        toggleButton.style.backgroundColor = silverColor;
-    }
-    isSilver = !isSilver; // меняем состояние
+    toggleButton.classList.toggle('gold');
+    toggleButton.classList.toggle('silver');
 });
