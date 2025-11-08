5d4cd0d84487f4856e21cf152b4567b60d39a0fc
saItist <tamerlanchapaev06062008@gmail.com>
2025-11-08 15:40:54 +0300

    link new js file completing task

---
+++ homework-5.js
@@ -0,0 +1,43 @@
+function showCityWeather(city, temperature) {
+    console.log(`Сейчас в ${city} температура - ${temperature} градусов по Цельсию.`);
+}
+
+showCityWeather("Махачкала", 16);
+
+showCityWeather("Киев", 6);
+
+const speedOfSound = 343
+
+function checkSpeed(speed) {
+    if (speed > speedOfSound) {
+        console.log("Сверхзвуковая скорость");
+    } else if (speed < speedOfSound) {
+        console.log("Дозвуковая скорость");
+    } else {
+        console.log("Скорость звука");
+    }
+}
+
+const product = "Ноутбук";
+const price = 1200;
+
+function checkPurchase(budget) {
+    if (budget >= price) {
+        console.log(`${product} приобретён. Спасибо за покупку!`);
+    } else {
+        const difference = price - budget;
+        console.log(`Вам не хватает ${difference}$, пополните баланс`);
+    }
+}
+
+
+let bookTitle = "1984";
+let author = "Джордж Оруэлл";
+let pages = 328;
+
+
+function showBookInfo() {
+    console.log(`Книга "${bookTitle}" написана автором ${author} и содержит ${pages} страниц.`);
+}
+
+showBookInfo();
